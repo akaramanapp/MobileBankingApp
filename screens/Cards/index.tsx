e2fd4f818f723338eb4b6288b78ede1036a1dcce
Abdulkerim Karaman <abdulkerimkaraman@gmail.com>
2022-04-24 23:44:34 +0300

    first commit

---
+++ screens/Cards/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Cards() {
+  return (
+    <View>
+      <Text>Cards</Text>
+    </View>
+  )
+}
---
+++ screens/index.tsx
@@ -0,0 +1,13 @@
+import Home from "./Home"
+import Settings from "./Settings"
+import Cards from "./Cards"
+import Transactions from "./Transactions"
+import MoneyTransfer from "./MoneyTransfer"
+
+export {
+    Home,
+    Settings,
+    Cards,
+    Transactions,
+    MoneyTransfer
+}
